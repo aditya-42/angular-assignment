9de0afdc199c628dfd4889f372d19060d33d7569
Aditya Purohit <aditya.s.purohit@gmail.com>
2025-03-27 11:13:10 -0400

    header and routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', title:'Home Page', component: HomeComponent },
+    { path: 'products', title:'Products', component: ProductsComponent },
+  ];
