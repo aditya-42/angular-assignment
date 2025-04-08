a10b5c1849adf4eac22f287e96965ffc67ef0915
Aditya Purohit <aditya.s.purohit@gmail.com>
2025-04-08 19:23:59 -0400

    api added

---
+++ src/app/api.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl = "https://fakestoreapi.com";
+
+  constructor(private http: HttpClient) { }
+
+  getProducts():Observable<any>{
+    const url = `${this.baseUrl}/products`;  // Endpoint for users
+    return this.http.get(url).pipe(
+      map(response => response),  
+      catchError(error => {
+        console.error('Error fetching products:', error);
+        throw error;
+      })
+    );
+  }
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { CommonModule } from '@angular/common';
+
+type products = {
+  title:string
+}
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  products:products[] = [];
+  loading: boolean = true;
+  error: string | null = null;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    // Fetch users when the component initializes
+    this.apiService.getProducts().subscribe(
+      (data) => {
+        this.products = data;
+        this.loading = false;  
+      },
+      (err) => {
+        this.error = 'Failed to fetch products. Please try again later.';
+        this.loading = false;
+        console.error(err);
+      }
+    );
+  }
+ 
 }
